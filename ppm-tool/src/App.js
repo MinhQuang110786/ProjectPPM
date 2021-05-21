3caa1312cbb4adc5fa4c067a166dbd3fa49f7b65
heavenlight86@gmail.com <heavenlight86@gmail.com>
2021-05-21 15:27:52 +0700

    update frontend

--- ppm-tool/src/App.js
+++ ppm-tool/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddProject from './components/Project/AddProject';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+
+      </div>
+
+    </Router>
+
   );
 }
 
---
+++ ppm-tool/src/components/Layout/Header.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Dashboard from '../Dashboard'
+export default class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+        <div className="container">
+          <Link className="navbar-brand" to="/dashboard">
+            Personal Project Management Tool
+            </Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/dashboard">Dashboard</a>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link " href="register.html">Sign Up</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="login.html">Login</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
